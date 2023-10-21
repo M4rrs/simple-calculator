2d73df7649fdf5c1f1bf7c389b7f7c05d6e21389
M4rrs <almasnorazman2803@outlook.com>
2023-10-21 22:54:47 +0800

    more styling

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="calculator">
-		<div className="output">
-			<div className="prev-op">12345</div>
-			<div className="curr-op">12345</div>
+  	<div className="wrap">
+		<div className="calculator">
+			<div className="output">
+				<div className="prev-op">12345</div>
+				<div className="curr-op"></div>
+			</div>
+			<button className='button-2'>AC</button>
+			<button>DEL</button>
+			<button>+</button>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+			<button>*</button>
+			<button>4</button>
+			<button>5</button>
+			<button>6</button>
+			<button>+</button>
+			<button>7</button>
+			<button>8</button>
+			<button>9</button> 
+			<button>-</button>
+			<button>0</button>
+			<button>.</button>
+			<button className='button-2'>=</button>
 		</div>
-		<button className='button-2'>AC</button>
-		<button>DEL</button>
-		<button>+</button>
-		<button>1</button>
-		<button>2</button>
-		<button>3</button>
-		<button>*</button>
-		<button>4</button>
-		<button>5</button>
-		<button>6</button>
-		<button>+</button>
-		<button>7</button>
-		<button>8</button>
-		<button>9</button> 
-		<button>-</button>
-		<button>0</button>
-		<button>.</button>
-		<button className='button-2'>=</button>
 	</div>
   );
 }
