c03144bb29bc3aba61abbdbf2c1f9f3d74f75676
M4rrs <almasnorazman2803@outlook.com>
2023-10-25 00:29:33 +0800

    this is hard

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-  	<div className="wrap">
-		<div className="calculator">
-			<div className="output">
-				<div className="prev-op">12345</div>
-				<div className="curr-op"></div>
+	const [state, setResult] = useState("");
+
+	const handleClick = (e) => {
+		setResult(state.concat(e.target.name));
+	}
+
+	return (
+		<div className="wrap">
+			<div className="calculator">
+				<div class="output">
+					{/* <div className="prev-op"></div> */}
+					<input type="text" className="curr-op" value={state}></input>
+				</div>
+				<button className='button-2'>AC</button>
+				<button>DEL</button>
+				<button name="/" onClick={handleClick}>/</button>
+				<button name="1" onClick={handleClick}>1</button>
+				<button name="2" onClick={handleClick}>2</button>
+				<button name="3" onClick={handleClick}>3</button>
+				<button name="*" onClick={handleClick}>*</button>
+				<button name="4" onClick={handleClick}>4</button>
+				<button name="5" onClick={handleClick}>5</button>
+				<button name="6" onClick={handleClick}>6</button>
+				<button name="+" onClick={handleClick}>+</button>
+				<button name="7" onClick={handleClick}>7</button>
+				<button name="8" onClick={handleClick}>8</button>
+				<button name="9" onClick={handleClick}>9</button> 
+				<button name="-" onClick={handleClick}>-</button>
+				<button name="0" onClick={handleClick}>0</button>
+				<button name="." onClick={handleClick}>.</button>
+				<button className='button-2'>=</button>
 			</div>
-			<button className='button-2'>AC</button>
-			<button>DEL</button>
-			<button>+</button>
-			<button>1</button>
-			<button>2</button>
-			<button>3</button>
-			<button>*</button>
-			<button>4</button>
-			<button>5</button>
-			<button>6</button>
-			<button>+</button>
-			<button>7</button>
-			<button>8</button>
-			<button>9</button> 
-			<button>-</button>
-			<button>0</button>
-			<button>.</button>
-			<button className='button-2'>=</button>
 		</div>
-	</div>
-  );
+	);
 }
 
 export default App;
